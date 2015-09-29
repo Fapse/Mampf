04d16a41356fb6773dc1eeb34b676ecfe0a06d71
Fapse <fb-online@posteo.de>
2015-09-29 16:09:54 +0200

    Some last changes for today!

--- Mampf/src/com/fapse/mampf/view/MampfController.java
+++ Mampf/src/com/fapse/mampf/view/MampfController.java
@@ -2,6 +2,7 @@ package com.fapse.mampf.view;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
@@ -11,12 +12,14 @@ public class MampfController {
 	
 	@FXML
 	private void initialize() {
-		System.out.println("test");
+		Label date1 = new Label("Mon, 12/12/2015");
+		dayGrid.add(date1, 0, 0);
 		dayGrid.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
 				System.out.println(event.getSource());
+				System.out.println(event.getPickResult());
 			}
 		});
 	}
