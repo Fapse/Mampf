302a0359e3c2e274c3b12b285faf6605153b02db
Fapse <fb-online@posteo.de>
2015-09-29 15:06:32 +0200

    added EventHandler

--- Mampf/src/com/fapse/mampf/view/MampfController.java
+++ Mampf/src/com/fapse/mampf/view/MampfController.java
@@ -1,15 +1,23 @@
 package com.fapse.mampf.view;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class MampfController {
 	@FXML
-	private GridPane gridPane;
+	private GridPane dayGrid;
 	
 	@FXML
-	private void initialize()
-	{
+	private void initialize() {
 		System.out.println("test");
+		dayGrid.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				System.out.println(event.getSource());
+			}
+		});
 	}
 }
