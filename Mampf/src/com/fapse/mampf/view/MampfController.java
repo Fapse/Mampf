dd0cda309ea78ec6d3cb6ab9bc26050f23c97f00
Fapse <fb-online@posteo.de>
2015-09-29 14:27:25 +0200

    ControllerClass is working

--- Mampf/src/com/fapse/mampf/view/MampfController.java
+++ Mampf/src/com/fapse/mampf/view/MampfController.java
@@ -1,10 +1,14 @@
 package com.fapse.mampf.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 
 public class MampfController {
 	@FXML
-	private void initializeGridPane()
+	private GridPane gridPane;
+	
+	@FXML
+	private void initialize()
 	{
 		System.out.println("test");
 	}
