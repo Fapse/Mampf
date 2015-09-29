4a3d1e54f45711128d1035b864bdc92a80840f8a
Fapse <fb-online@posteo.de>
2015-09-29 12:05:46 +0200

    completed MampfApp, first run successful

--- Mampf/src/com/fapse/mampf/MampfApp.java
+++ Mampf/src/com/fapse/mampf/MampfApp.java
@@ -1,23 +1,29 @@
 package com.fapse.mampf;
 
 import javafx.application.Application;
-import javafx.scene.layout.StackPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MampfApp extends Application {
 
 	private Stage stage;
-	private StackPane rootLayout;
+	private GridPane rootLayout;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage stage) throws Exception {
+		this.stage = stage;
+		this.stage.setTitle("Mampf");
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(MampfApp.class.getResource("view/MampfOverview.fxml"));
+		rootLayout = (GridPane) loader.load();
+		Scene scene = new Scene(rootLayout);
+		this.stage.setScene(scene);
+		this.stage.show();		
 	}
-
 }
