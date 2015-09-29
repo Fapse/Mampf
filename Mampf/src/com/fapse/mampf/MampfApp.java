7b53c6fc9f95ef8a8401faf0fc6ad9e1e07b35d9
Fapse <fb-online@posteo.de>
2015-09-29 11:32:42 +0200

    Declared Stage, Stack pane, Declared start()

--- Mampf/src/com/fapse/mampf/MampfApp.java
+++ Mampf/src/com/fapse/mampf/MampfApp.java
@@ -1,10 +1,23 @@
 package com.fapse.mampf;
 
-public class MampfApp {
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MampfApp extends Application {
+
+	private Stage stage;
+	private StackPane rootLayout;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
