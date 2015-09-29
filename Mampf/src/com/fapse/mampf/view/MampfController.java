1d0fdd60095cbdeabad9e7216b6b0ca48771b74d
Fapse <fb-online@posteo.de>
2015-09-29 20:06:11 +0200

    Playing with UI possibilities

--- Mampf/src/com/fapse/mampf/view/MampfController.java
+++ Mampf/src/com/fapse/mampf/view/MampfController.java
@@ -1,5 +1,6 @@
 package com.fapse.mampf.view;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import javafx.event.EventHandler;
@@ -10,6 +11,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class MampfController {
 	private GridPane dayGrid = new GridPane();
@@ -21,19 +24,35 @@ public class MampfController {
 	private void initialize() {
 		
 		//ArrayList<Label> labelList = new ArrayList<Label>();
-		
+		int days = 0;
 		for(int row = 0; row < 7; row++) {
 			dayGrid.getColumnConstraints().add(new ColumnConstraints(120));
 			dayGrid.getRowConstraints().add(new RowConstraints(120));
 			for(int col = 0; col < 7; col++) {
-				Label label = new Label("Test_" + row + col);
-				dayGrid.add(label, col, row);
-				label.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				LocalDate date = LocalDate.now();
+				date = date.plusDays(days++);
+				Text text = new Text(date.toString());
+				TextFlow textFlow = new TextFlow(text);
+				dayGrid.add(textFlow, col, row);
+				textFlow.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 
 					@Override
 					public void handle(MouseEvent event) {
-						System.out.println(event.getSource());
-						System.out.println(event.getPickResult());
+						if (event.getSource() instanceof TextFlow) {
+							TextFlow tf = (TextFlow) event.getSource();
+							System.out.println(tf.getChildren().get(0));
+						}
+											}
+				});
+				
+				text.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+					@Override
+					public void handle(MouseEvent event) {
+						if (event.getSource() instanceof Text) {
+							Text text = (Text) event.getSource();
+							System.out.println(text.toString());
+						}
 					}
 				});
 			}
