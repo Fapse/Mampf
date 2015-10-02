301152df48e5c64dd7e2b298bd0d1ebbd83b28ae
Fapse <fb-online@posteo.de>
2015-10-02 16:55:00 +0200

    Chaos und Verzweiflung

--- Mampf/src/com/fapse/mampf/MampfApp.java
+++ Mampf/src/com/fapse/mampf/MampfApp.java
@@ -1,11 +1,10 @@
 package com.fapse.mampf;
 
-import java.util.Date;
-
+import java.time.LocalDate;
+import com.fapse.mampf.model.Meal;
 import com.fapse.mampf.model.Recipe;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,7 +15,16 @@ public class MampfApp extends Application {
 
 	private Stage stage;
 	private BorderPane rootLayout;
-	private ObservableMap<Date, Recipe> mealAgenda = FXCollections.checkedObservableMap(null, null, null);
+	//private ObservableMap<LocalDate, Recipe> mealAgenda = FXCollections.checkedObservableMap(null, null, null);
+	private LocalDate day1 = LocalDate.of(1977, 5, 31);
+	private Recipe spatzen = new Recipe("Käsespatzen", "Rühren, hobeln, kochen");
+	private Recipe pommes = new Recipe("Pommes", "Schnippeln, fritieren");
+	private Meal mittagessen = new Meal(spatzen, day1);
+	
+	public MampfApp() {
+		System.out.println("Die Mahlzeit ist: " + mittagessen.getRecipeName());
+		
+	}
 
 	public static void main(String[] args) {
 		launch(args);
--- Mampf/src/com/fapse/mampf/model/Meal.java
+++ Mampf/src/com/fapse/mampf/model/Meal.java
@@ -1,5 +1,46 @@
 package com.fapse.mampf.model;
 
-public class Meal {
+import java.time.LocalDate;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
+public class Meal {
+	private SimpleStringProperty recipeName;
+	private Recipe recipe;
+	private final ObjectProperty<LocalDate> date;
+	public Meal(Recipe recipe, LocalDate date) {
+		this.recipe = recipe;
+		this.date = new SimpleObjectProperty<LocalDate>(date);
+		StringProperty sp = recipe.nameProperty();
+		System.out.println("Hier gehts: " + sp);
+		this.recipeName.bind(recipe.nameProperty());
+		System.out.println("Hier gehts nimmer!");
+	}
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	public Recipe getRecipe() {
+		return recipe;
+	}
+	public StringProperty recipeNameProperty() {
+		return recipeName;
+	}
+	public String getRecipeName() {
+		return recipeName.get();
+	}
+	public ObjectProperty<LocalDate> dateProperty() {
+		return date;
+	}
+	public String getDate() {
+		return date.toString();
+	}
+	public void setDate(LocalDate date) {
+		try {
+			this.date.set(date);
+		} catch (Exception e) {
+			//ignore
+		}
+	}
 }
