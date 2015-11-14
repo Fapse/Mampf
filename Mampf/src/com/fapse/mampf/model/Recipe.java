660b7194ac618157e9361d147b7a4ca93e659c17
Fapse <fb-online@posteo.de>
2015-11-14 13:55:30 +0100

    Some new features in Recipe.java
    Added rating, effort, bestBefore + image Strings
    
    Signed-off-by: Fapse <fb-online@posteo.de>

--- Mampf/src/com/fapse/mampf/model/Recipe.java
+++ Mampf/src/com/fapse/mampf/model/Recipe.java
@@ -5,18 +5,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe implements Serializable {
-	private static final long serialVersionUID = -6503331288632008128L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1476949188820906150L;
 	private String uid;
 	private String name;
 	private String recipe;
+	private String rating;
+	private String effort;
+	private String bestBefore;
+	private String image;
+	
 	private List<Condiment> condiments = new ArrayList<>();
 	
 	public Recipe(String... args) {
-		if (((args.length - 3) % 3) == 0) {
+		if (((args.length - 7) % 3) == 0) {
 			int counter = 0;
 			uid = args[counter++];
 			name = args[counter++];
 			recipe = args[counter++];
+			rating =  args[counter++];
+			effort = args[counter++];
+			bestBefore = args[counter++];
+			image = args[counter++];
 			while (counter < args.length) {
 				condiments.add(new Condiment(args[counter++], args[counter++], args[counter++]));
 			}
